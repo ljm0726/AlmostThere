d4402090e6a53db52938849b911e5af95d9a9d1f
안승태 <sng_a@naver.com>
2023-04-20 13:49:08 +0900

    feat: 검색 page 구현

--- frontend/src/components/PlacePage/SearchPlacePage.vue
+++ frontend/src/components/PlacePage/SearchPlacePage.vue
@@ -1,11 +1,239 @@
 <template>
-  <div></div>
+  <div class="map-area">
+    <v-btn
+      id="square-btn"
+      class="back-btn"
+      outlined
+      :color="color"
+      @click="goBack()"
+      rounded
+    >
+      <v-icon>$vuetify.icons.arrow_left</v-icon>
+    </v-btn>
+    <form action="">
+      <input
+        class="search-box"
+        placeholder="   모임장소를 검색하세요"
+        v-model="searchValue"
+        autocomplete="off"
+      />
+      <button id="submit_btn" @click.prevent="getSearchResult"></button>
+    </form>
+    <div class="map_section">
+      <div id="list" class="lists" v-show="isListOpen"></div>
+    </div>
+
+    <v-btn class="find-place-btn"
+      ><i class="fa-light fa-location-dot"></i>중간 위치 찾기</v-btn
+    >
+  </div>
 </template>
 
 <script>
 export default {
   name: "PlacePage",
+  data() {
+    return {
+      map: null,
+      ps: null,
+      current: { lat: 37.5, lng: 127.039 },
+      searchValue: "",
+      geocoder: null,
+      dong: "",
+      regCode: "",
+      residentResults: [],
+      isResident: false,
+      isListOpen: false,
+    };
+  },
+
+  mounted() {
+    this.loadScript();
+  },
+
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+    loadScript() {
+      const script = document.createElement("script");
+      script.src =
+        "//dapi.kakao.com/v2/maps/sdk.js?appkey=4a440970d2ed6adb820352f0223f931f&autoload=false&libraries=services"; // &autoload=false api를 로드한 후 맵을 그리는 함수가 실행되도록 구현
+      script.onload = () => window.kakao.maps.load(this.loadMap); // 스크립트 로드가 끝나면 지도를 실행될 준비가 되어 있다면 지도가 실행되도록 구현
+
+      document.head.appendChild(script); // html>head 안에 스크립트 소스를 추가
+    },
+    getSearchResult() {
+      // 검색 결과
+      this.ps = new window.kakao.maps.services.Places();
+      this.geocoder = new window.kakao.maps.services.Geocoder();
+      console.log("search", this.searchValue);
+      if (this.marker) this.marker.setMap(null);
+      this.ps.keywordSearch(this.searchValue, this.placesSearchCB);
+    },
+    placesSearchCB(data, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        console.log("s", data);
+        this.isResident = false;
+        this.residentResults = [];
+        const listDiv = data.reduce(
+          (
+            cur,
+            { place_url, place_name, category_name, y, x, address_name }
+          ) => {
+            console.log("카테고리 : ", category_name);
+            const temp = address_name.split(" ");
+            console.log("주소", temp);
+            this.residentResults.push({
+              place_url,
+              place_name,
+              category_name,
+              y,
+              x,
+              address_name,
+            });
+            this.isResident = true;
+            return (
+              cur +
+              `
+                <div class="resident_items" data-x=${x} data-y=${y} data-place=${place_name} data-address="${address_name}" >${address_name} ${place_name}</div>
+              `
+            );
+          },
+          ""
+        );
+        if (this.isResident) {
+          const list = document.querySelector("#list");
+          list.innerHTML = listDiv;
+          console.log(list.childNodes);
+          this.isListOpen = true;
+          list.childNodes.forEach((child) => {
+            child.addEventListener("click", () => {
+              console.log(
+                child.attributes["data-x"].value,
+                child.attributes["data-y"].value
+              );
+              const x = child.attributes["data-x"].value;
+              const y = child.attributes["data-y"].value;
+              this.address = child.attributes["data-address"].value;
+              // this.address = child.attributes["data-address"].value;
+              console.log("da", child.attributes["data-address"].value);
+              this.place = child.attributes["data-place"].value;
+              this.introduceValue = this.address;
+              var bounds = new window.kakao.maps.LatLngBounds();
+              bounds.extend(new window.kakao.maps.LatLng(y, x));
+              this.current.lng = x;
+              this.current.lat = y;
+              this.searchAddrFromCoords(this.current, this.getAddressFromRes);
+              this.displayMarker({ y, x });
+              // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+              this.map.setBounds(bounds);
+              this.map.relayout();
+              this.map.setCenter(
+                new window.kakao.maps.LatLng(this.current.lat, this.current.lng)
+              );
+            });
+          });
+        } else {
+          this.searchValue = "";
+        }
+      } else {
+        alert("검색 결과가 없습니다.");
+      }
+    },
+    searchAddrFromCoords(coords, callback) {
+      console.log(coords);
+      // 좌표로 행정동 주소 정보를 요청합니다
+      this.geocoder.coord2RegionCode(coords.lng, coords.lat, callback);
+    },
+    getAddressFromRes(result, status) {
+      // 주소 정보 파싱
+      console.log(this.map);
+      if (status === window.kakao.maps.services.Status.OK) {
+        for (var i = 0; i < result.length; i++) {
+          // 법정동 region_type 값은 'B' 이므로
+          console.log("result address", result);
+          if (result[i].region_type === "B") {
+            this.currentAddress = result[i].address_name;
+            const temp = this.currentAddress.split(" ");
+            this.currentPrevAddress = `${temp[0]} ${temp[1]}`;
+            this.dong = temp[2];
+            console.log(result[i].code);
+            this.regCode = result[i].code;
+            break;
+          }
+        }
+        console.log("sc", this.regCode);
+        // this.getRecentPrice(); => 가장 최근 가격
+      } else {
+        this.currentAddress = "실패";
+      }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+}
+.search_bar {
+  height: 5vh;
+}
+.search_bar form {
+  display: flex;
+  justify-content: space-between;
+}
+.search_bar form #search {
+  height: 5vh;
+  width: 80%;
+  border-radius: 10px 0 0 10px;
+}
+.search_bar form #search:focus {
+  border: 0;
+}
+.search_bar form #submit_btn {
+  width: 20%;
+  border-style: none;
+  background: black;
+  border-radius: 0 10px 10px 0;
+}
+.search_bar form #submit_btn #submit_btn_icon {
+}
+.map_section {
+  height: 200px;
+  display: flex;
+  gap: 10px;
+}
+.map_section .maps {
+  width: 100%;
+  height: 100%;
+}
+.map_section .lists {
+  overflow: scroll;
+}
+.map_section .resident_items {
+  font-size: 10px;
+  border-bottom: 1px solid #000;
+  padding: 2px;
+}
+.map_section .resident_items:hover {
+}
+input,
+textarea,
+#focus {
+  border: none;
+  border-right: 0px;
+  border-top: 0px;
+  border-left: 0px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.19);
+  background: #f8f8f8;
+  width: 100%;
+  display: block;
+}
+</style>
