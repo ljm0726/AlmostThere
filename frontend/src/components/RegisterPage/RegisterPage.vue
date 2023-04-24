e7950629e3fc3e5c1419687395760fddf7b9e071
l6778 <l6778@naver.com>
2023-04-24 14:37:37 +0900

    feat: meeting register css 및Date Picker 적용

--- frontend/src/components/RegisterPage/RegisterPage.vue
+++ frontend/src/components/RegisterPage/RegisterPage.vue
@@ -1,15 +1,228 @@
 <template>
-  <div>
-    등록페이지
+  <div style="margin: 0px 16px">
+    <div
+      class="point-font xxxxxxl-font main-col-1"
+      style="margin: 20% 0 13% 5%"
+    >
+      <p>많은 사람들과</p>
+      <p>모임을</p>
+      <p>잡아보세요!</p>
+      <img
+        src="@/assets/images/page/register.png"
+        height="450"
+        style="text-aling: center"
+        alt=""
+      />
+    </div>
     <!-- nav 자리 -->
-    <v-sheet height="56px"></v-sheet>
+    <v-card rounded="xl" elevation="5">
+      <v-card-title>
+        <!-- <img src="@/assets/images/dialog/logout.png" width="60%" /> -->
+        <!-- <span class="point-font xxxxl-font main-col-1 text-left">제목</span>
+        <v-text-field dense></v-text-field>
+        <span class="point-font xxxxl-font main-col-1 text-left">일시</span>
+        <span class="point-font xxxxl-font main-col-1 text-left">장소</span>
+        <v-text-field dense></v-text-field> -->
+      </v-card-title>
+      <v-card-text style="padding: 38px 24px 0 24px">
+        <!-- <img src="@/assets/images/dialog/logout.png" width="60%" /> -->
+        <span class="point-font xxxxl-font main-col-1 text-left">제목</span>
+        <v-text-field dense v-model="meetingname"></v-text-field>
+        <span class="point-font xxxxl-font main-col-1 text-left">일시</span>
+        <!-- <v-text-field dense></v-text-field> -->
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          max-width="280px"
+          min-width="280px"
+          :nudge-right="40"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="날짜"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              hide-details
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+            <v-btn text color="primary" @click="$refs.menu.save(date)">
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-menu>
+
+        <v-menu
+          ref="menu2"
+          v-model="menu2"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          :return-value.sync="time"
+          transition="scale-transition"
+          offset-y
+          max-width="280px"
+          min-width="280px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="time"
+              label="시간"
+              prepend-icon="mdi-clock-time-four-outline"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-time-picker
+            v-if="menu2"
+            v-model="time"
+            full-width
+            @click:minute="$refs.menu2.save(time)"
+            format="24hr"
+          ></v-time-picker>
+        </v-menu>
+
+        <span class="point-font xxxxl-font main-col-1 text-left">장소</span>
+        <v-text-field dense v-model="meetingplace"></v-text-field>
+
+        <v-row>
+          <v-col class="pr-1">
+            <v-btn
+              elevation="0"
+              color="var(--main-col-1)"
+              dark
+              rounded
+              block
+              @click="regist_meeting"
+              >등록</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
+// 추가해야할 부분 -> 현재 시간 이전을 설정하면 불가능하게
 export default {
   name: "RegisterPage",
+  data() {
+    return {
+      meetingname: null,
+      date: null,
+      menu: null,
+      time: null,
+      menu2: null,
+      meetingplace: "",
+      curDate: null,
+      curTime: null,
+      getCurMiliTimes: null,
+    };
+  },
+  methods: {
+    regist_meeting() {
+      this.getCurTime();
+
+      console.log(this.curDate, this.date, this.curTime, this.time);
+      if (this.curDate >= this.date && this.curTime >= this.time) {
+        alert("이전 시간 불가능!");
+      } else {
+        console.log(
+          "Input value : ",
+          this.meetingname,
+          this.date,
+          this.time,
+          this.meetingplace,
+          this.getCurMiliTimes
+        );
+
+        // API 연결 하기
+      }
+    },
+
+    getCurTime() {
+      const currentDate = new Date();
+      const options = {
+        timeZone: "Asia/Seoul",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      };
+      const dateArray = currentDate
+        .toLocaleDateString("en-GB", options)
+        .split("/");
+      this.curDate = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+      this.curTime = currentDate.toLocaleTimeString("en-GB", {
+        timeZone: "Asia/Seoul",
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    },
+  },
+
+  mounted() {
+    // this.date = new Date().toLocaleDateString();
+    this.getCurTime();
+    const currentDate = new Date();
+    const options = {
+      timeZone: "Asia/Seoul",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    };
+    const dateArray = currentDate
+      .toLocaleDateString("en-GB", options)
+      .split("/");
+    this.date = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`;
+    this.time = currentDate.toLocaleTimeString("en-GB", {
+      timeZone: "Asia/Seoul",
+      hour12: false,
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+p {
+  margin: -3% 0%;
+  font-size: 25px;
+}
+
+img {
+  position: absolute;
+  width: 184px;
+  height: 200px;
+  z-index: 100;
+  right: 16px;
+  top: 3%;
+}
+
+.v-input {
+  /* width: 95%; */
+  text-align: left;
+}
+.pr-1 {
+  padding: 12px;
+}
+.row {
+  /* width: 100%; */
+  padding-right: 3%;
+}
+
+#menu2 {
+  margin-top: -100px;
+}
+</style>
