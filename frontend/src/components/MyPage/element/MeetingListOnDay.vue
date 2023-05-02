6f71ebd2cc24c4d2f4cd8cea724038f7a72878e4
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-02 15:17:10 +0900

    feat: 선택한 날짜에 모임이 있을 경우 로직 구현

--- frontend/src/components/MyPage/element/MeetingListOnDay.vue
+++ frontend/src/components/MyPage/element/MeetingListOnDay.vue
@@ -2,7 +2,7 @@
   <v-sheet
     >모임 리스트 {{ date }}
     <div v-if="meetings.length == 0"><no-meeting /></div>
-    <div v-else>모임있음</div>
+    <div v-else>모임있음 {{ this.meetings }}</div>
   </v-sheet>
 </template>
 
@@ -27,7 +27,29 @@ export default {
   computed: {
     ...mapState("memberStore", ["attendMeetings"]),
   },
-  methods: {},
+  watch: {
+    date() {
+      this.getOnDayMeetings();
+    },
+  },
+  created() {},
+  methods: {
+    getOnDayMeetings() {
+      console.log("#21# 해당 날짜 모임조회 동작");
+
+      const onDayMeeting = [];
+      this.meetings = this.attendMeetings.map((meeting) => {
+        const meetingDate = meeting.meetingDto.meetingTime.substring(0, 10);
+        // console.log("#21# 모임날짜 vs 선택한 날짜: ", meetingDate, this.date);
+
+        // 현재 선택한 날짜의 모임 조회
+        if (meetingDate == this.date) {
+          onDayMeeting.push(meeting.meetingDto);
+        }
+      });
+      this.meetings = onDayMeeting;
+    },
+  },
 };
 </script>
 
