b0cbeec82904bec0f216a37de43d3cd0709c95a6
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-02 11:05:30 +0900

    design: 글씨 색상 & 굵기 조정

--- frontend/src/components/MyPage/element/MemberSummary.vue
+++ frontend/src/components/MyPage/element/MemberSummary.vue
@@ -12,15 +12,17 @@
       rounded="xl"
     >
       <!-- text -->
-      <div class="light-font main-col-1">
+      <div class="light-font">
         <!-- 이번달 모임개수 -->
         <div class="d-flex align-center mb-1 mt-3">
           <span
             ><img height="15" width="15" src="@/assets/images/dialog/check.png"
           /></span>
           <span class="ml-2"
-            >이번달에 <b>{{ this.thisMonthAttendMeetingCnt }}번</b>의 모임을
-            잡으셨네요.</span
+            >이번달에
+            <b class="main-col-1 bold-font"
+              >{{ this.thisMonthAttendMeetingCnt }}번</b
+            >의 모임을 잡으셨네요.</span
           >
         </div>
         <!-- 누적 지각횟수 -->
@@ -29,8 +31,12 @@
             ><img height="15" width="15" src="@/assets/images/dialog/check.png"
           /></span>
           <span class="ml-2"
-            >누적 <b>{{ this.attendMeetings.length }}번</b>의 약속 중
-            <b>{{ this.totalLateCnt }}번</b> 지각하셨습니다.</span
+            >누적
+            <b class="main-col-1 bold-font"
+              >{{ this.attendMeetings.length }}번</b
+            >의 약속 중
+            <b class="main-col-1 bold-font">{{ this.totalLateCnt }}번</b>
+            지각하셨습니다.</span
           >
         </div>
         <!-- 지난달 총 소비금액 -->
@@ -40,7 +46,8 @@
           /></span>
           <span class="ml-2"
             >지난달 모임에
-            <b>{{ this.lastMonthTotalSpentMoney.toLocaleString("ko-KR") }}원</b
+            <b class="main-col-1 bold-font"
+              >{{ this.lastMonthTotalSpentMoney.toLocaleString("ko-KR") }}원</b
             >을 소비했습니다.</span
           >
         </div>
