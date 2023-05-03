571d08c1ec5d789c06c548f28949e03174e80e48
dyeon7310 <dyeon7310@gmail.com>
2023-05-03 15:24:57 +0900

    feat: 모임 상세 보기 구현

--- backend/src/main/java/com/almostThere/domain/meeting/dto/detail/MeetingDetailResponseDto.java
+++ backend/src/main/java/com/almostThere/domain/meeting/dto/detail/MeetingDetailResponseDto.java
@@ -1,10 +1,11 @@
 package com.almostThere.domain.meeting.dto.detail;
 
 import com.almostThere.domain.meeting.entity.Meeting;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,11 +22,14 @@ public class MeetingDetailResponseDto {
     private Integer lateAmount;
     private LocalDateTime regDate;
     private String roomCode;
+    private String startPlace;
+    private String startAddress;
+    private Integer spentMoney;
     private List<MeetingMemberResponseDto> meetingMembers;
-    private List<MeetingCalculateDetailDto> meetingCalculateDetails;
+    private List<MeetingCalculateDetailDto> calculateDetails;
 
-    public MeetingDetailResponseDto(Meeting meeting
-        , List<MeetingMemberResponseDto> meetingMembers, List<MeetingCalculateDetailDto> meetingCalculateDetails) {
+    public MeetingDetailResponseDto(Meeting meeting, String startPlace, String startAddress, Integer spentMoney
+        , List<MeetingMemberResponseDto> meetingMembers, List<MeetingCalculateDetailDto> calculateDetails) {
         this.hostId = meeting.getHost().getId();
         this.meetingId = meeting.getId();
         this.meetingName = meeting.getMeetingName();
@@ -37,7 +41,10 @@ public class MeetingDetailResponseDto {
         this.lateAmount = meeting.getLateAmount();
         this.regDate = meeting.getRegdate();
         this.roomCode = meeting.getRoomCode();
+        this.startPlace = startPlace;
+        this.startAddress = startAddress;
+        this.spentMoney = spentMoney;
         this.meetingMembers = meetingMembers;
-        this.meetingCalculateDetails = meetingCalculateDetails;
+        this.calculateDetails = calculateDetails;
     }
 }
--- backend/src/main/java/com/almostThere/domain/meeting/service/MeetingService.java
+++ backend/src/main/java/com/almostThere/domain/meeting/service/MeetingService.java
@@ -1,11 +1,11 @@
 package com.almostThere.domain.meeting.service;
 
+import com.almostThere.domain.meeting.dto.MeetingDto;
+import com.almostThere.domain.meeting.dto.create.MeetingCreateRequestDto;
 import com.almostThere.domain.meeting.dto.delete.MeetingDeleteRequestDto;
 import com.almostThere.domain.meeting.dto.detail.MeetingCalculateDetailDto;
-import com.almostThere.domain.meeting.dto.create.MeetingCreateRequestDto;
 import com.almostThere.domain.meeting.dto.detail.MeetingDetailRequestDto;
 import com.almostThere.domain.meeting.dto.detail.MeetingDetailResponseDto;
-import com.almostThere.domain.meeting.dto.MeetingDto;
 import com.almostThere.domain.meeting.dto.detail.MeetingMemberResponseDto;
 import com.almostThere.domain.meeting.dto.update.MeetingUpdateRequestDto;
 import com.almostThere.domain.meeting.entity.Meeting;
@@ -18,14 +18,14 @@ import com.almostThere.domain.user.repository.MemberRepository;
 import com.almostThere.global.error.ErrorCode;
 import com.almostThere.global.error.exception.AccessDeniedException;
 import com.almostThere.global.error.exception.NotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -103,18 +103,22 @@ public class MeetingService {
      * @return 모임 상세정보
      */
     public MeetingDetailResponseDto getMeetingDetail(MeetingDetailRequestDto meetingDetailRequestDto){
+        Long memberId = meetingDetailRequestDto.getMemberId();
         final Meeting meeting = meetingRepository.findById(meetingDetailRequestDto.getMeetingId())
             .orElseThrow(()->new NotFoundException(
                 ErrorCode.MEETING_NOT_FOUND));
         List<MeetingMemberResponseDto> meetingMembers = meeting.getMeetingMembers()
             .stream().map(m->new MeetingMemberResponseDto(m)).collect(Collectors.toList());
+        MeetingMember member = meeting.getMeetingMembers().stream().filter(m->m.getMember().getId()==memberId).findAny().get();
+
         List<MeetingCalculateDetailDto> calculateDetails = new ArrayList<>();
         if(meeting.getCalculateDetails() != null) {
             calculateDetails = meeting.getCalculateDetails()
                 .stream().map(c->new MeetingCalculateDetailDto(c)).collect(Collectors.toList());
         }
 
-        return new MeetingDetailResponseDto(meeting,meetingMembers,calculateDetails);
+        return new MeetingDetailResponseDto(meeting, member.getStartPlace(), member.getStartAddress(),
+                member.getSpentMoney(), meetingMembers, calculateDetails);
     }
 
     /**
