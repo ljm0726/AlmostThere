1307bfcf34cc0726ef9457d6f5c3f6463a3c24ab
dyeon7310 <dyeon7310@gmail.com>
2023-05-04 10:32:33 +0900

    feat: 서버 파일 삭제 로직 추가

--- backend/src/main/java/com/almostThere/global/util/FileUtil.java
+++ backend/src/main/java/com/almostThere/global/util/FileUtil.java
@@ -34,4 +34,27 @@ public class FileUtil {
         result.put("fileName",name);
         return result;
     }
+    public void fileDelete(String hostname, String fileName){
+        //서버에 파일 저장
+        String path = "";
+        File file = null;
+
+        if (hostname.substring(0, 7).equals("DESKTOP")) {
+            path = "C:/almostthere/";
+            file = new File(path + fileName);
+        } else {
+            path = "/var/lib/almostthere/";
+            file = new File(path + fileName);
+        }
+
+        if(file.exists()){
+            if(file.delete()){
+                System.out.println("파일삭제 성공");
+            }else{
+                System.out.println("파일삭제 실패");
+            }
+        }else{
+            System.out.println("파일이 존재하지 않습니다.");
+        }
+    }
 }
