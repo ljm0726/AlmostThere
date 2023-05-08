c1c6761bc808396a2380c71ab36b9c1596633da5
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-08 15:44:15 +0900

    feat: 모임시간 3시간 전 페이지 진입 불가 일부구현

--- frontend/src/components/MeetingPage/button/MoveLiveMapBtn.vue
+++ frontend/src/components/MeetingPage/button/MoveLiveMapBtn.vue
@@ -7,9 +7,35 @@
 <script>
 export default {
   name: "MoveLiveMapBtn",
+  props: {
+    meetingTime: String,
+  },
+  data() {
+    return {
+      threeHoursAgoFlag: false, // 모임 시간 3시간 전 check value
+    };
+  },
   methods: {
     moveLivemap() {
-      this.$router.push(`/live-map/${this.$route.params.id}`);
+      this.checkMeetingTime();
+
+      if (this.threeHoursAgoFlag) {
+        this.$router.push(`/live-map/${this.$route.params.id}`);
+      } else {
+        // 위치공유 페이지로 이동 불가
+      }
+    },
+    // [@Method] 모임시간 3시간 전 check
+    checkMeetingTime() {
+      const meetingTime = new Date(this.meetingTime);
+      const threeHoursAgoTime = new Date(
+        meetingTime.getTime - 3 * 60 * 60 * 1000
+      );
+      const currentTime = new Date();
+
+      if (currentTime.getTime() >= threeHoursAgoTime.getTime()) {
+        this.threeHoursAgoFlag = true;
+      }
     },
   },
 };
