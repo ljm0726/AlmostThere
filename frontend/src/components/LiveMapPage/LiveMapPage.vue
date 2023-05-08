ff2b760adf2ec435edf8166042459968ce102fe4
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-08 15:30:31 +0900

    comment: test 로그인 code 삭제

--- frontend/src/components/LiveMapPage/LiveMapPage.vue
+++ frontend/src/components/LiveMapPage/LiveMapPage.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="live-map">
-    <!-- <v-btn @click="ssafyLogin()">싸피 로그인</v-btn>
-    <v-btn @click="heeLogin()">희정 로그인</v-btn>
-    <v-btn @click="seoLogin()">서찰스 로그인</v-btn> -->
     <live-map></live-map>
   </div>
 </template>
@@ -13,34 +10,6 @@ import LiveMap from "./element/LiveMap.vue";
 export default {
   name: "LiveMapPage",
   components: { LiveMap },
-  // 임시, TEST용
-  // methods: {
-  //   ssafyLogin() {
-  //     // 현 로그인한 member의 정보
-  //     // console.log("#21# ssafy 로그인");
-  //     const memberStore = {
-  //       memberId: 1,
-  //       memberNickName: "김싸피",
-  //     };
-  //     localStorage.setItem("member", JSON.stringify(memberStore));
-  //   },
-  //   heeLogin() {
-  //     // console.log("#21# hee 로그인");
-  //     const memberStore = {
-  //       memberId: 2,
-  //       memberNickName: "희정",
-  //     };
-  //     localStorage.setItem("member", JSON.stringify(memberStore));
-  //   },
-  //   seoLogin() {
-  //     // console.log("#21# hee 로그인");
-  //     const memberStore = {
-  //       memberId: 3,
-  //       memberNickName: "찰스",
-  //     };
-  //     localStorage.setItem("member", JSON.stringify(memberStore));
-  //   },
-  // },
 };
 </script>
 
