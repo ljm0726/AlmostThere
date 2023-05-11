8254fb4b311fb853b2d6d0007d69ec4d612d893f
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-11 12:51:07 +0900

    feat: map 배경화면 변경 버튼 일부구현

--- frontend/src/components/LiveMapPage/LiveMapPage.vue
+++ frontend/src/components/LiveMapPage/LiveMapPage.vue
@@ -16,6 +16,7 @@
     </v-sheet>
     <!-- kakao map -->
     <div v-else class="live-map">
+      <change-background-btn />
       <!-- <div class="live-map" v-if="enterTimeCheckFlag"> -->
       <arrive-and-chat-btn
         @resizeMapLevel="resizeMapLevel"
@@ -40,6 +41,7 @@
 <script>
 import LiveMap from "./element/LiveMap.vue";
 import ArriveAndChatBtn from "./element/ArriveAndChatBtn.vue";
+import ChangeBackgroundBtn from "./element/ChangeBackgroundBtn.vue";
 import NoEnterTimeDialog from "./element/NoEnterTimeDialog.vue";
 import InternetError from "@/common/component/dialog/InternetError.vue";
 import { liveMapInfo } from "@/api/modules/livemap.js";
@@ -49,6 +51,7 @@ export default {
   components: {
     LiveMap,
     ArriveAndChatBtn,
+    ChangeBackgroundBtn,
     NoEnterTimeDialog,
     InternetError,
   },
---
+++ frontend/src/components/LiveMapPage/element/ChangeBackgroundBtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-sheet color="transparent">
+    <v-sheet
+      max-width="500"
+      color="transparent"
+      width="100%"
+      class="d-flex flex-column justify-end align-end"
+      style="
+        position: fixed;
+        margin: 0 auto;
+        left: 0;
+        right: 0;
+        bottom: 82%;
+        padding: 0 15px 15px 0;
+        z-index: 2;
+      "
+    >
+      <!-- <v-btn class="ml-1 mt-1" color="var(--main-col-1)" dark rounded>
+        배경
+      </v-btn> -->
+      <v-switch></v-switch>
+    </v-sheet>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "ChangeBackground",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
