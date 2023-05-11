c42e1ebb691e2ac6636fa4794259f2e356f8b615
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-05-11 14:57:36 +0900

    feat: access_token 재발급 후 요청하였던 api 재요청

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -10,13 +10,17 @@ function apiInstance() {
     },
   });
 
-  const api = instance;
+  // const api = instance;
 
   instance.interceptors.request.use(
     function (config) {
       // Do something before request is sent
       const Authorization = localStorage.getItem("Authorization");
       if (Authorization) {
+        console.log(
+          "#[interceptor_request]# Authorization 추가: ",
+          Authorization
+        );
         config.headers.Authorization = Authorization;
       }
       return config;
@@ -28,41 +32,38 @@ function apiInstance() {
   );
 
   let isRefreshFlag = false;
-
   instance.interceptors.response.use(
     function (response) {
       console.log(response);
       return response;
     },
     async function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
-
       const errorAPI = error.config;
+
+      // i) access_token 재발급
       if (!isRefreshFlag) {
-        console.log("에러일 경우", error.response.data);
         if (
           error.response.data.status === 401 &&
           errorAPI.retry === undefined
         ) {
+          // console.log("#[interceptor_response]# access_token 재발급");
           errorAPI.retry = true;
           isRefreshFlag = true;
-          console.log("토큰이 이상한 오류일 경우");
 
-          await api
-            .post(`${process.env.VUE_APP_API_BASE_URL}` + "/token/tokenReissue")
+          await instance
+            .post(`/token/tokenReissue`)
             .then((response) => {
-              console.log(response);
+              // console.log("#[access_token 재발급]# 성공 response: ", response);
               localStorage.setItem("Authorization", response.data);
-
-              console.log(errorAPI);
             })
             .catch((error) => {
-              console("무슨에러?", error);
+              console.log("#[access_token 재발급]# 실패 error: ", error);
             });
 
-          return await axios(errorAPI);
-        } else {
+          return await instance(errorAPI);
+        }
+        // ii) 재로그인 권유
+        else {
           console.log("#interceptor# 로그인 페이지로 이동");
           // this.$router.push(name: "/login");
         }
