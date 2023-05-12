f674daf1731d3652d0d4317cf32e5db2a932b710
Jeeyoun-S <dreaming990124@gmail.com>
2023-05-12 21:50:17 +0900

    feat: meeting 페이지 로더 구현

--- frontend/src/components/MeetingPage/MeetingLoading.vue
+++ frontend/src/components/MeetingPage/MeetingLoading.vue
@@ -15,8 +15,8 @@
         <div class="d-flex flex-row justify-end">
           <v-skeleton-loader
             type="image"
-            width="30"
-            height="30"
+            width="35"
+            height="35"
           ></v-skeleton-loader>
         </div>
         <div class="d-flex flex-row justify-space-between align-end">
@@ -24,32 +24,64 @@
             <img height="35" width="35" src="@/assets/images/logo/logo.png" />
             <v-skeleton-loader
               type="image"
-              width="200"
-              height="30"
+              width="150"
+              height="35"
             ></v-skeleton-loader>
           </div>
           <div class="d-flex flex-row">
             <v-skeleton-loader
               class="mr-2"
               type="image"
-              width="30"
-              height="30"
+              width="35"
+              height="35"
             ></v-skeleton-loader>
             <v-skeleton-loader
               type="image"
-              width="30"
-              height="30"
+              width="35"
+              height="35"
             ></v-skeleton-loader>
           </div>
         </div>
       </v-sheet>
     </v-sheet>
-
-    <!-- <div class="d-flex flex-column"> -->
-    <div v-for="n in 4" :key="n" class="mx-3 my-2">
+    <div class="mx-3 my-2">
       <v-skeleton-loader
         type="image"
-        width="100"
+        width="60"
+        height="30"
+      ></v-skeleton-loader>
+      <div class="d-flex flex-row justify-space-between">
+        <v-skeleton-loader
+          class="my-1"
+          type="image"
+          width="49%"
+          height="40"
+        ></v-skeleton-loader>
+        <v-skeleton-loader
+          class="my-1"
+          type="image"
+          width="49%"
+          height="40"
+        ></v-skeleton-loader>
+      </div>
+    </div>
+    <div class="mx-3 my-2">
+      <v-skeleton-loader
+        type="image"
+        width="60"
+        height="30"
+      ></v-skeleton-loader>
+      <v-skeleton-loader
+        class="my-1"
+        type="image"
+        width="100%"
+        height="50"
+      ></v-skeleton-loader>
+    </div>
+    <div class="mx-3 my-2">
+      <v-skeleton-loader
+        type="image"
+        width="60"
         height="30"
       ></v-skeleton-loader>
       <v-skeleton-loader
@@ -58,8 +90,27 @@
         width="100%"
         height="80"
       ></v-skeleton-loader>
+      <v-skeleton-loader
+        class="my-1"
+        type="image"
+        width="100%"
+        height="30"
+        style="border-radius: 15px"
+      ></v-skeleton-loader>
     </div>
-    <!-- </div> -->
+    <div class="mx-3 my-2">
+      <v-skeleton-loader
+        type="image"
+        width="60"
+        height="30"
+      ></v-skeleton-loader>
+      <v-skeleton-loader
+        class="my-1"
+        type="image"
+        width="100%"
+        height="50"
+      ></v-skeleton-loader>
+    </div>
   </v-sheet>
 </template>
 
