eede68f647d083f373d07d3f34cf475225ba396a
Jeeyoun-S <dreaming990124@gmail.com>
2023-05-12 21:40:42 +0900

    feat: meeting 페이지 로더 구현

--- frontend/src/components/MeetingPage/MeetingLoading.vue
+++ frontend/src/components/MeetingPage/MeetingLoading.vue
@@ -1,63 +1,65 @@
 <template>
-  <v-sheet
-    min-height="100%"
-    class="d-flex flex-column justify-end mx-3"
-    style="padding: 55px 0px 20px 0px"
-  >
-    <div class="align-self-end d-flex flex-column align-end">
+  <v-sheet>
+    <v-sheet
+      class="pa-3"
+      style="
+        background-image: linear-gradient(to bottom, #092a49 75%, white 25%);
+      "
+    >
+      <v-sheet
+        class="pa-6 d-flex flex-column justify-space-between detail-shadow detail-border"
+        height="190"
+        outlined
+        rounded="xl"
+      >
+        <div class="d-flex flex-row justify-end">
+          <v-skeleton-loader
+            type="image"
+            width="30"
+            height="30"
+          ></v-skeleton-loader>
+        </div>
+        <div class="d-flex flex-row justify-space-between align-end">
+          <div>
+            <img height="35" width="35" src="@/assets/images/logo/logo.png" />
+            <v-skeleton-loader
+              type="image"
+              width="200"
+              height="30"
+            ></v-skeleton-loader>
+          </div>
+          <div class="d-flex flex-row">
+            <v-skeleton-loader
+              class="mr-2"
+              type="image"
+              width="30"
+              height="30"
+            ></v-skeleton-loader>
+            <v-skeleton-loader
+              type="image"
+              width="30"
+              height="30"
+            ></v-skeleton-loader>
+          </div>
+        </div>
+      </v-sheet>
+    </v-sheet>
+
+    <!-- <div class="d-flex flex-column"> -->
+    <div v-for="n in 4" :key="n" class="mx-3 my-2">
       <v-skeleton-loader
         type="image"
-        class="mb-1"
-        width="230"
-        height="30"
-      ></v-skeleton-loader>
-      <v-skeleton-loader
-        type="image"
-        class="mb-1"
         width="100"
         height="30"
       ></v-skeleton-loader>
+      <v-skeleton-loader
+        class="my-1"
+        type="image"
+        width="100%"
+        height="80"
+      ></v-skeleton-loader>
     </div>
-    <div class="d-flex flex-row">
-      <v-skeleton-loader type="avatar" class="mb-1 mr-2"></v-skeleton-loader>
-      <div>
-        <v-skeleton-loader
-          type="image"
-          class="mb-1"
-          width="100"
-          height="30"
-        ></v-skeleton-loader>
-        <v-skeleton-loader
-          type="image"
-          class="mb-1"
-          width="200"
-          height="30"
-        ></v-skeleton-loader>
-      </div>
-    </div>
-    <v-skeleton-loader
-      type="image"
-      class="align-self-end mb-1"
-      width="70"
-      height="30"
-    ></v-skeleton-loader>
-    <div class="d-flex flex-row">
-      <v-skeleton-loader type="avatar" class="mb-1 mr-2"></v-skeleton-loader>
-      <div>
-        <v-skeleton-loader
-          type="image"
-          class="mb-1"
-          width="180"
-          height="30"
-        ></v-skeleton-loader>
-        <v-skeleton-loader
-          type="image"
-          class="mb-1"
-          width="250"
-          height="30"
-        ></v-skeleton-loader>
-      </div>
-    </div>
+    <!-- </div> -->
   </v-sheet>
 </template>
 
